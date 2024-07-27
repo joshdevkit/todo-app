64bdfbf6e4c526c3b421c7b9469de7f930cb329f
Joshua Pacho <102531192+joshdevkit@users.noreply.github.com>
2024-07-27 15:19:00 +0800

    Updates

---
+++ static/js/952.b3aaf90f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_app_supabase=self.webpackChunkreact_app_supabase||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.b3aaf90f.chunk.js.map
